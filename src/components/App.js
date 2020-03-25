cee6785d0cacde615afb7ddafeeed8f3fec5b8b8
Scott <Scotthall1983@hotmail.com>
2020-03-25 11:42:38 +0000

    Created Video List and Video Item components. Linked them together to ensure that the link item is returning from a search.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = "AIzaSyBLbb8DAJ6eaz0By47QZrKmx2mjiSslP-g";
 
 class App extends React.Component {
-  state = {videos: []}
+  state = { videos: [] };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -16,15 +17,15 @@ class App extends React.Component {
         maxResults: 5
       }
     });
-    console.log(response)
-    this.setState({ videos: response.data.items})
+    console.log(response);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
